107f27d7d6e3aa2d64b3ab4ea7c0d5a68fda07c0
Filip <Filipspencer@gmail.com>
2026-09-13 12:21:53 +0100

    .

--- Main.py
+++ Main.py
@@ -10,18 +10,13 @@ mainwindow = Tk()
 canvas = Canvas()
 mainwindow.title("Project")
 mainwindow.geometry("600x400")
+image1 = PhotoImage(file="dry-clean.png")
 
-class circle():
-    def __init__(self,name,day,row1,column1):
-        self.name = name
-        self.day = day
-        self.row = row1
-        self.column = column1
-        ttk.Button(mainwindow, text="1",command=self.run()).grid(row=row1,column=column1)
-    def run():
-        pass
 ttk.Button(mainwindow, text="Quit", command=mainwindow.destroy).pack(anchor="ne",side="right")
 ttk.Button(mainwindow, text="Help", ).pack(anchor="nw",side="left")
 ttk.Button(mainwindow, text="Settings", ).pack(anchor="nw",side="left")
+
+ttk.Button(mainwindow, text="Quit", command=mainwindow.destroy,image=image1).pack(anchor="ne",side="right")
+
 canvas.pack(anchor="center")
 mainwindow.mainloop()
